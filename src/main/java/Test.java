bc9937e425b7d23f11826662d247152c29eee39c
janguni <lightson23@naver.com>
2023-03-30 16:29:01 +0900

    fix: test.java

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -2,11 +2,13 @@ public class Test {
     String name = "name";
     int age = 23;
 
-    int new_age;
+    int age_changed;
 
-    new_age = change_age(age);
+    public Test() {
+        age_changed = change_age(age);
+    }
 
     int change_age(int age) {
-        return age+1
+        return age+1;
     }
 }
